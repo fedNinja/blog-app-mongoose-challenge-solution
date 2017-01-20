48b7eb4a680acdadbd9dd0cd04e49d8d3a74637e
payal agrawal <pagrawal312@gmail.com>
2017-01-19 22:05:12 -0800

    added test for end points with mongoose

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -3,9 +3,10 @@ const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
 const should = chai.should();
+const ObjectID = require('mongodb').ObjectID;
 const {BlogPost} = require('../models');
 const {app, runServer, closeServer} = require('../server');
-const {TEST_DATABASE_URL} = require('../config');
+const {DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
@@ -43,7 +44,7 @@ function tearDownDb() {
 describe('Blog-Posts API resource', function() {
 
   before(function() {
-    return runServer(TEST_DATABASE_URL);
+    return runServer(DATABASE_URL);
   });
 
   beforeEach(function() {
@@ -56,7 +57,7 @@ describe('Blog-Posts API resource', function() {
 
   after(function() {
     return closeServer();
-  })
+  });
 
   
   describe('GET endpoint', function() {
@@ -65,19 +66,22 @@ describe('Blog-Posts API resource', function() {
       return chai.request(app)
         .get('/posts')
         .then(function(_res) {
-          res = _res;
-          res.should.have.status(200);
+          //console.log(_res);
+          _res.should.have.status(200);
+          res = _res.body;
+           //console.log(res);
           // otherwise our db seeding didn't work
-          res.body.posts.should.have.length.of.at.least(1);
+          res.should.have.length.of.at.least(1);
           return BlogPost.count();
         })
         .then(function(count) {
-          res.body.posts.should.have.length.of(count);
+         res.should.have.length.of(count);
         });
     });
+  });
 
 
-    it('should return blog posts with right fields', function() {
+   it('should return blog posts with right fields', function() {
 
       let resBlogPosts;
       return chai.request(app)
@@ -85,26 +89,28 @@ describe('Blog-Posts API resource', function() {
         .then(function(res) {
           res.should.have.status(200);
           res.should.be.json;
-          res.body.posts.should.be.a('array');
-          res.body.posts.should.have.length.of.at.least(1);
+          res.body.should.be.a('array');
+          res.body.should.have.length.of.at.least(1);
 
-          res.body.posts.forEach(function(post) {
+        res.body.forEach(function(post) {
             post.should.be.a('object');
             post.should.include.keys(
               'id', 'author', 'title', 'content');
           });
-          resBlogPosts = res.body.posts[0];
-          return BlogPost.findById(resBlogPosts.id);
+            resBlogPosts = res.body[0];
+            console.log(resBlogPosts);
+            return BlogPost.findById(resBlogPosts.id);
         })
         .then(function(post) {
-          resBlogPosts.id.should.equal(post.id);
-          resBlogPosts.author.firstName.should.equal(post.author.firstName);
-          resBlogPosts.author.lastName.should.equal(post.author.lastName);
+          console.log(post);
+          let objectId = new ObjectID(resBlogPosts.id);
+          console.log(objectId.equals(post._id));
+          objectId.equals(post._id).should.equal(true);
+          resBlogPosts.author.should.equal(post.author.firstName +" "+ post.author.lastName);
           resBlogPosts.title.should.equal(post.title);
           resBlogPosts.content.should.equal(post.content);
         });
     });
-  });
 
   describe('POST endpoint', function() {
     it('should add a new blog posts', function() {
@@ -118,7 +124,7 @@ describe('Blog-Posts API resource', function() {
           res.body.should.be.a('object');
           res.body.should.include.keys(
             'id', 'author', 'title', 'content');
-          res.body.author.should.equal(newBlogPost.author);
+          res.body.author.should.equal(newBlogPost.author.firstName +" "+ newBlogPost.author.lastName);
           // cause Mongo should have created id on insertion
           res.body.id.should.not.be.null;
           res.body.content.should.equal(newBlogPost.content);
@@ -152,11 +158,11 @@ describe('Blog-Posts API resource', function() {
           updateData.id = post.id;
 
           return chai.request(app)
-            .put(`/posts/${posts.id}`)
+            .put(`/posts/${post.id}`)
             .send(updateData);
         })
         .then(function(res) {
-          res.should.have.status(204);
+          res.should.have.status(201);
 
           return BlogPost.findById(updateData.id).exec();
         })
@@ -178,7 +184,7 @@ describe('Blog-Posts API resource', function() {
         .exec()
         .then(function(_post) {
           post = _post;
-          return chai.request(app).delete(`/restaurants/${post.id}`);
+          return chai.request(app).delete(`/posts/${post.id}`);
         })
         .then(function(res) {
           res.should.have.status(204);
