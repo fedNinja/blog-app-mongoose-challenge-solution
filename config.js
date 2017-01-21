8c436fcbd8ff7e5fabc5e54394c35b8dc4dee5be
payal agrawal <pagrawal312@gmail.com>
2017-01-20 19:03:14 -0800

    chnaged production var

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL =process.env.DATABASE_URL ||
                       global.DATABASE_URL ||
                       (process.env.NODE_ENV === 'production' ?
-                           'mongodb://mongoose_user:payal123@ds049538.mlab.com:49538/blog-mongoose' :
+                           'mongodb://blog-mongoose:payal123@ds117889.mlab.com:17889/blog-mongoose' :
                            'mongodb://localhost/blog-app');
 exports.PORT = process.env.PORT || 8080;
