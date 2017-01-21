1782819ef3a9911d8fa8f7c7ed733c2126587311
payal agrawal <pagrawal312@gmail.com>
2017-01-20 19:00:27 -0800

    changed db name in mongoose production

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL =process.env.DATABASE_URL ||
                       global.DATABASE_URL ||
                       (process.env.NODE_ENV === 'production' ?
-                           'mongodb://mongoose_user:payal123@ds049538.mlab.com:49538/blog-app' :
+                           'mongodb://mongoose_user:payal123@ds049538.mlab.com:49538/blog-mongoose' :
                            'mongodb://localhost/blog-app');
 exports.PORT = process.env.PORT || 8080;
